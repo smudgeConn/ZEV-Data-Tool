591367fc4d80a5a4c1ec82120a4607e6e4a3b1af
Sandi Connors, M.A., CCC-SLP <123496920+smudgeConn@users.noreply.github.com>
2023-10-25 16:22:50 -0400

    Update SEV-GUI-OOP.py
    Worked on trying to make the drop down lists connected to one another and limit hard coding of inputs.

--- SEV-GUI-OOP.py
+++ SEV-GUI-OOP.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import sample_vehicle_objects as svo
 
 
 class App(tk.Tk):
@@ -25,28 +26,73 @@ class App(tk.Tk):
 
 
 class FileSelection(ttk.Frame):
-    def __init__(self, container, label_text='File Selection', vehicle_list=['Lucid Air', 'EV Hummer', 'F150 Lightning'], test_list=['001', '002', '003'], source_list=['Dyno', 'CAN'], sample_list=['N01', 'N02', 'N03', 'N04'], button_text='Load Data'):
+    def __init__(self, container, label_text='File Selection', vehicle_list=['Lucid', 'Tesla'], test_list=['001', '002'], data_source_list=['Dyno', 'CAN'], sample_list=['N01', 'N02'], button_text='Load Data'):
         super().__init__(container)
-        self.selected_vehicle = tk.StringVar()
-        self.selected_test = tk.StringVar()
 
+        # variables
+        self.label_text = label_text
+        self.button_text = button_text
+        self.vehicle_list = vehicle_list
+        self.vehicle_var = tk.StringVar()
+        self.test_list = test_list
+        self.test_var = tk.StringVar()
+        self.data_source_list = data_source_list
+        self.data_source_var = tk.StringVar()
+        self.sample_list = sample_list
+        self.sample_var = tk.StringVar()
+
+        # place the frame in the root window
         self.grid(row=0, rowspan=3, column=0, columnspan=2,
                   padx=5, pady=5, ipadx=5, ipady=5, sticky="nsew")
 
         # widgets
         ttk.Label(self, text=label_text).pack()
 
-        #  Vehicle OptionMenu -- IN: list of strings; OUT: vehicle name as string
-        self.selected_vehicle.set(vehicle_list[0])
-        self.vehicle_option_menu = ttk.OptionMenu(
-            self, self.selected_vehicle, *vehicle_list)
-        self.vehicle_option_menu.pack()
+        # vehicle menu
+        self.vehicle_menu = ttk.Combobox(
+            self, textvariable=self.vehicle_var)
+        self.vehicle_menu['values'] = self.vehicle_list
+        self.vehicle_menu.pack()
+        self.vehicle_menu.bind("<<ComboboxSelected>>", self.update_tests)
+        self.vehicle_menu.set("Select a Vehicle")
 
-        #  Test OptionMenu -- IN: list of strings; OUT: test name as string
-        self.test_option_menu = ttk.OptionMenu(
-            self, self.selected_test, *test_list)
+        # test menu
+        self.test_menu = ttk.Combobox(self, textvariable=self.test_var)
+        self.test_menu['values'] = self.test_list
+        self.test_menu.pack()
+        self.test_menu.bind("<<ComboboxSelected>>", self.update_data_sources)
+        self.test_menu.set("Select a Test")
 
-        ttk.Button(self, text=button_text).pack()
+        # data source menu
+        self.data_source_menu = ttk.Combobox(
+            self, textvariable=self.data_source_var)
+        self.data_source_menu['values'] = self.data_source_list
+        self.data_source_menu.pack()
+        self.data_source_menu.bind(
+            "<<ComboboxSelected>>", self.update_samples)
+        self.data_source_menu.set("Select a Data Source")
+
+        # sample menu
+        self.sample_menu = ttk.Combobox(self, textvariable=self.sample_var)
+        self.sample_menu['values'] = self.sample_list
+        self.sample_menu.pack()
+        self.sample_menu.set("Select a Sample")
+
+        # load data button
+        self.load_data_button = tk.Button(
+            self, text=button_text, command=self.load_data).pack()
+
+    def update_tests(self, selection):
+        self.test_list = ['new test', 'new test2']
+
+    def update_data_sources(self, selection):
+        self.data_source_list = ['new data source', 'new data source2']
+
+    def update_samples(self, selection):
+        self.sample_list = ['new sample', 'new sample2']
+
+    def load_data(self):
+        pass
 
 
 class SignalSelection(ttk.Frame):
@@ -55,9 +101,6 @@ class SignalSelection(ttk.Frame):
         ttk.Label(self, text="Signal Selection").pack()
         self.grid(row=0, rowspan=3, column=2, columnspan=1,
                   padx=5, pady=5, ipadx=5, ipady=5, sticky="nsew")
-
-    def create_widgets(self):
-        pass
 
 
 class GraphDisplay(ttk.Frame):
